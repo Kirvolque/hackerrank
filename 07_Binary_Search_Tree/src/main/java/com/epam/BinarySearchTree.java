8a9b09287f9a6c2aab4639e8e0e38243d33469a7
Kirill Volkov <neborodulin@gmail.com>
2017-07-29 23:13:07 +0300

    binary tree fixed

--- 07_Binary_Search_Tree/src/main/java/com/epam/BinarySearchTree.java
+++ 07_Binary_Search_Tree/src/main/java/com/epam/BinarySearchTree.java
@@ -14,6 +14,20 @@ public class BinarySearchTree {
         return checkHelper(root, new HashSet<Integer>());
     }
 
+    int maxInSubtree(Node node){
+        while (node.right != null){
+            node = node.right;
+        }
+        return node.data;
+    }
+
+    int minInSubtree(Node node){
+        while (node.left != null){
+            node = node.left;
+        }
+        return node.data;
+    }
+
     boolean checkHelper(Node node, HashSet<Integer> set) {
 
         if (node == null){
@@ -24,10 +38,10 @@ public class BinarySearchTree {
         }
 
         set.add(node.data);
-        if (node.left != null && (node.data >= node.right.data)){
+        if (node.left != null && (node.data <= maxInSubtree(node.left))){
             return false;
         }
-        if (node.right != null && (node.data <= node.left.data)){
+        if (node.right != null && (node.data >= minInSubtree(node.right))){
             return false;
         }
         return (checkHelper(node.left, set) && checkHelper(node.right, set));
