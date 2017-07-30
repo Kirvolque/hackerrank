4a7865f7b2d5e2e98a7d406d5f03fefe11494adc
Kirill Volkov <neborodulin@gmail.com>
2017-07-31 01:39:20 +0300

    merge sort fixed

--- 12_Merge_Sort/src/main/java/com/epam/MergeSort.java
+++ 12_Merge_Sort/src/main/java/com/epam/MergeSort.java
@@ -1,54 +1,51 @@
 package com.epam;
 
-//import java.util.Scanner;
-
-//import static com.epam.MergeSort.merge;
+import java.util.Scanner;
 
 public class MergeSort {
 
-    public static int swaps;
-
     public static void main(String[] args) {
-//        Scanner in = new Scanner(System.in);
-//        int t = in.nextInt();
-//        for(int a0 = 0; a0 < t; a0++){
-//            int n = in.nextInt();
-//            int arr[] = new int[n];
-//            for(int arr_i=0; arr_i < n; arr_i++){
-//                arr[arr_i] = in.nextInt();
-//            }
-//            sort(arr, n);
-//        }
-
-        int [] arr = {1, 1 ,1, 2, 2};
-        sort (arr, 5);
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int arr[] = new int[n];
+            for(int arr_i=0; arr_i < n; arr_i++){
+                arr[arr_i] = in.nextInt();
+            }
+            sort(arr, n);
+        }
 
     }
 
     public static void sort(int[] data, int size) {
-        swaps = 0;
-        mergeSortDown(data, 0, size, new int[size]);
-        System.out.println(swaps);
+
+        System.out.println(mergeSortDown(data, 0, size, new int[size]));
     }
 
-    private static void mergeSortDown(int[] data, int startInclusive, int endExclusive, int[] additional) {
+    private static long mergeSortDown(int[] data, int startInclusive, int endExclusive, int[] additional) {
 
         final int length = endExclusive - startInclusive;
         if (length <= 1) {
-            return;
+            return 0;
         }
 
         final int mid = startInclusive + length / 2;
-        mergeSortDown(data, startInclusive, mid, additional);
-        mergeSortDown(data, mid, endExclusive, additional);
 
-        merge(data, startInclusive, mid, endExclusive, additional);
+        long swaps = 0L;
+        swaps += mergeSortDown(data, startInclusive, mid, additional);
+        swaps += mergeSortDown(data, mid, endExclusive, additional);
+
+        swaps += merge(data, startInclusive, mid, endExclusive, additional);
+
+        return swaps;
     }
 
-    private static void merge(int[] data, int startInclusive, int mid, int endExclusive, int[] additional){
+    private static long merge(int[] data, int startInclusive, int mid, int endExclusive, int[] additional){
         System.arraycopy(data, startInclusive, additional, startInclusive, endExclusive - startInclusive);
         int i = startInclusive;
         int j = mid;
+        long swaps = 0L;
         for (int k = startInclusive; k < endExclusive; k++) {
             if (i >= mid){
                 data[k] = additional[j++];
@@ -60,11 +57,14 @@ public class MergeSort {
             else if (additional[i] <= additional[j]){
                 data[k] = additional[i++];
             }
+
             else {
                 swaps += mid - i;
                 data[k] = additional[j++];
             }
         }
+        return swaps;
     }
+
 
 }
