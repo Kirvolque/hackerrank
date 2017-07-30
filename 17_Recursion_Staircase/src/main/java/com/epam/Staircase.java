01df4e89764609054b53f2c28715c0725f221198
Kirill Volkov <neborodulin@gmail.com>
2017-07-31 02:27:31 +0300

    optimized

--- 17_Recursion_Staircase/src/main/java/com/epam/Staircase.java
+++ 17_Recursion_Staircase/src/main/java/com/epam/Staircase.java
@@ -4,17 +4,22 @@ import java.util.Scanner;
 
 public class Staircase {
 
-
+    static int[] count;
 
     public static int calculateWaysOfClimpbing(int n){
         if (n < 0) return 0;
         if (n == 0) return 1;
         if (n == 1) return 1;
 
+
         int sum = 0;
+        if (count[n - 1] != 0){
+            return count[n - 1];
+        }
         for (int k = 1; k <= 3; k++){
             sum += calculateWaysOfClimpbing(n - k);
         }
+        count[n - 1] = sum;
         return sum;
 
     }
@@ -24,7 +29,7 @@ public class Staircase {
         int s = in.nextInt();
         for(int a0 = 0; a0 < s; a0++){
             int n = in.nextInt();
-            //calculateWaysOfClimpbing(n, 0, n);
+            count = new int[n];
             System.out.println(calculateWaysOfClimpbing(n));
         }
     }
