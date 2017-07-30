42f9ba777522e4e616e137693098d53140b7a5e2
Kirill Volkov <neborodulin@gmail.com>
2017-07-31 02:15:09 +0300

    staircase task solved

---
+++ 17_Recursion_Staircase/src/main/java/com/epam/Staircase.java
@@ -0,0 +1,30 @@
+package com.epam;
+
+import java.util.Scanner;
+
+public class Staircase {
+
+
+
+    public static int calculateWaysOfClimpbing(int n){
+        if (n < 0) return 0;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        int sum = 0;
+        for (int k = 0; k <= 3; k++){
+            sum += calculateWaysOfClimpbing(n - k);
+        }
+        return sum;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        for(int a0 = 0; a0 < s; a0++){
+            int n = in.nextInt();
+            //calculateWaysOfClimpbing(n, 0, n);
+            System.out.println(calculateWaysOfClimpbing(n));
+        }
+    }
+}
