3e53dc17247868ac39936036ea902ff6df3198f0
Kirill Volkov <neborodulin@gmail.com>
2017-08-03 03:53:47 +0300

    DFS first commit

---
+++ 14_DFS/src/main/java/com/epam/Solution.java
@@ -0,0 +1,54 @@
+package com.epam;
+
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static class point{
+        int x;
+        int y;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            point point = (point) o;
+
+            if (x != point.x) return false;
+            return y == point.y;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = x;
+            result = 31 * result + y;
+            return result;
+        }
+    }
+
+
+    public static int sizeOfRegion(int x, int y, HashSet){
+
+    }
+    
+
+    public static int getBiggestRegion(int[][] matrix) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int grid[][] = new int[n][m];
+        for(int grid_i=0; grid_i < n; grid_i++){
+            for(int grid_j=0; grid_j < m; grid_j++){
+                grid[grid_i][grid_j] = in.nextInt();
+            }
+        }
+        System.out.println(getBiggestRegion(grid));
+    }
+}
